d852ea79ff4c0cd11985c7ccb82f7f16c7a323cc
azharie <azharie@staff.gramedia.com>
2018-11-06 21:06:48 +0700

    add new action: create company

--- src/actions/company.js
+++ src/actions/company.js
@@ -19,17 +19,42 @@ export const getCompanyFailure = (error) => {
 
 export const getCompany = () => {
   console.log('wooowjoijoijo')
-  return dispatch => {
-    console.log('wowowowowowo')
+  return (dispatch, getState) => {
+    return companyService().getCompany().then(res => {
+      if (res.status === 200) {
+        return dispatch(getCompanySuccess(res.data));
+      }
+    })
+    .catch(() => {
+      return dispatch(getCompanyFailure({error: 'Oops! Something went wrong and we couldn\'t get list of companies'}));
+    });
   }
-  // return (dispatch, getState) => {
-  //   return companyService.getCompany().then(res => {
-  //     if (res.status === 200) {
-  //       return dispatch(getCompanySuccess(res.data));
-  //     }
-  //   })
-  //   .catch(() => {
-  //     return dispatch(getCompanyFailure({error: 'Oops! Something went wrong and we couldn\'t get list of companies'}));
-  //   });
-  // }
+}
+
+export const createCompanySuccess = (company) => {
+  console.log('dapat nin ', company)
+  return {
+    type: types.CREATE_COMPANY_SUCCESS,
+    payload: {company}
+  }
+}
+
+export const createCompanyFailure = (error) => {
+  return {
+    type: types.CREATE_COMPANY_FAILURE,
+    error
+  }
+}
+
+export const createCompany = (data) => {
+  return (dispatch, getState) => {
+    return companyService().createCompany(data).then((res) => {
+      if (res.status === 200) {
+        return dispatch(createCompanySuccess(res.data))
+      }
+    })
+    .catch(() => {
+      return dispatch(createCompanyFailure({error: 'Oops! Something went wrong and we couldn\'t get list of companies'}));
+    })
+  }
 }
--- src/reducers/company.js
+++ src/reducers/company.js
@@ -3,15 +3,18 @@ import * as types from '../types';
 
 
 const initialState = {
-  companies: ["digshogi"]
+  companies: []
 }
 
 function company(state=initialState, action) {
-  console.log('woo', action.type)
   switch (action.type) {
     case types.GET_COMPANY_SUCCESS: 
       return { ...state, companies: action.payload.companies}
     case types.GET_COMPANY_FAILURE:
+      return action.payload.error
+    case types.CREATE_COMPANY_SUCCESS:
+      return { ...state, companies: action.payload.company}
+      case types.CREATE_COMPANY_FAILURE:
       return action.payload.error
     default: 
       return state
